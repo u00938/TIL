e4dd580e0f77cbfb35cd39ec2e49304e3a7c7e8e
u00938 <yo01030184262@gmail.com>
2021-04-28 19:52:46 +0900

    TIL

---
+++ TDD/practice/server.js
@@ -0,0 +1,31 @@
+const express = require('express');
+
+// Constants
+const PORT = 5000;
+
+// App
+const app = express();
+const productRoutes = require('./routes')
+const mongoose = require('mongoose');
+
+const user = 'databaseuser';
+const password = 'young4262';
+const db = 'ttd';
+
+mongoose.connect(`mongodb+srv://${user}:${password}@cluster0.1weod.mongodb.net/${db}?retryWrites=true&w=majority`, 
+  {
+    useNewUrlParser: true, // 경고 문구 뜨지 않게
+    useUnifiedTopology: true
+  })
+  .then(() => console.log('Mongodb Connected...'))
+  .catch(err => console.log(err));
+
+app.use(express.json());
+
+app.use('/api/products', productRoutes)
+
+app.get('/', (req, res) => {
+  res.send('Hello')
+})
+
+app.listen(PORT, () => console.log(`Running on port ${PORT}`));
